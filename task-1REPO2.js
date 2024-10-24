0357f41374f8cfcc5d2a9dcf37cfb17917cd0aa9
dennis <dennis19876@googlemail.com>
2024-10-24 13:53:56 +0200

    aufgaben repo 2 done

--- task-1REPO2.js
+++ task-1REPO2.js
@@ -1,21 +1,55 @@
 // Task 1
 
-const user1 = {};
-const user2 = {};
-const user3 = {};
+const user1 = {
+  firstName: "Mark",
+  lastName: "Maxwell",
+  age: 32,
+  email: "mark@email.com",
+};
+const user2 = {
+  firstName: "Sally",
+  lastName: "Samson",
+  age: 24,
+  email: "sally@email.com",
+};
+const user3 = {
+  firstName: "Jenny",
+  lastName: "Jay",
+  age: 38,
+  email: "jenny@email.com",
+};
 
-console.log(user1);
-console.log(user2);
-console.log(user3);
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
 
 // Task 1.1
 
+user1.city = "Berlin";
+user2.city = "London";
+user3.city = "Paris";
+
+// console.log(user1);
+
 // Task 1.2
 
-function describe() {}
+function describe(user) {
+  return `${user.firstName} ${user.lastName} is ${user.age} years old and lives in New York`;
+}
+
+// console.log(describe(user1));
 
 // Task 1.3
 
-function showJob() {}
+user2.job = "web developer";
+user1.job = "architect";
 
-// test änderung
+function showJob(user) {
+  if (user.job) {
+    return `${user.firstName} ${user.lastName} works as a ${user.job}`;
+  } else {
+    return `${user.firstName} ${user.lastName} is currently unemployed`;
+  }
+}
+
+console.log(showJob(user3));
--- task-2REPO2.js
+++ task-2REPO2.js
@@ -1,8 +1,23 @@
 // Task 2
 
-const vehicle1 = {};
-const vehicle2 = {};
-const vehicle3 = {};
+const vehicle1 = {
+  type: "car",
+  color: "green",
+  brand: "honda",
+  mileage: 1000,
+};
+const vehicle2 = {
+  type: "truck",
+  color: "yellow",
+  brand: "toyota",
+  mileage: 10000,
+};
+const vehicle3 = {
+  type: "van",
+  color: "orange",
+  brand: "ford",
+  mileage: 100000,
+};
 
 console.log(vehicle1);
 console.log(vehicle2);
@@ -10,10 +25,10 @@ console.log(vehicle3);
 
 // Task 2.1
 
-function needsRevision() {}
+function needsRevision(vehicle) {
+  return vehicle.mileage > 60000;
+}
 
 console.log(`Does vehicle1 need revision?`, needsRevision(vehicle1));
 console.log(`Does vehicle2 need revision?`, needsRevision(vehicle2));
 console.log(`Does vehicle3 need revision?`, needsRevision(vehicle3));
-
-// test änderung
